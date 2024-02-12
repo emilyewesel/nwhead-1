7f130aa39eca73f673ff52d3e48addb3e28c42d7
emilyewesel <ewesel@stanford.edu>
2024-02-12 15:50:34 -0800

    Update train.py

--- train.py
+++ train.py
@@ -451,7 +451,7 @@ def eval_epoch(val_loader, network, criterion, optimizer, args, mode='random'):
 
 def fc_step(batch, network, criterion, optimizer, args, is_train=True):
     '''Train/val for one step.'''
-    img, label, gender = batch
+    img, label = batch
     img = img.float().to(args.device)
     label = label.to(args.device)
     optimizer.zero_grad()
@@ -472,7 +472,7 @@ def fc_step(batch, network, criterion, optimizer, args, is_train=True):
 
 def nw_step(batch, network, criterion, optimizer, args, is_train=True, mode='random'):
     '''Train/val for one step.'''
-    img, label, gender = batch
+    img, label = batch
     img = img.float().to(args.device)
     label = label.to(args.device)
     optimizer.zero_grad()
