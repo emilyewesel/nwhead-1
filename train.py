129b949d40311501dafa2edf41ce6c68ef12bfc3
emilyewesel <ewesel@stanford.edu>
2024-04-18 22:06:25 -0700

    Update train.py

--- train.py
+++ train.py
@@ -38,9 +38,11 @@ class ChexpertDataset(Dataset):
         
         if train_class == "Cardiomegaly" and correct_support_only and train:
             self.df_fc_results = pd.read_csv(fc_results)
+            print("emily fc", fc_results.head())
             merged = pd.merge(self.df, self.df_fc_results, on='Path', how='inner')
+            print("emily merged", merged.head())
             filtered_df = merged[merged['Ground Truth'] == merged['Prediction']]
-            print(filtered_df.head())
+            print("emily filtered", filtered_df.head())
             self.df = filtered_df
 
         if train_class == "No Finding":
