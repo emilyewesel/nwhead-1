2f681e9bc6d1580ea818ef3c42262e202b472b33
emilyewesel <ewesel@stanford.edu>
2024-04-20 16:55:44 -0700

    Update train.py

--- train.py
+++ train.py
@@ -305,12 +305,13 @@ def main():
         baase2 = "/dataNAS/people/paschali/datasets/chexpert-public/chexpert-public/"
         fc_head_results = "/dataNAS/people/ewesel1/nwhead-1/saved_models/methodfchead_datasetchexpert_archresnet18_pretrainedFalse_lr0.0001_bs64_projdim0_nshot8_nwayNone_wd0.0001_seed1964_classCardiomegaly/output_csv/train_model_output_epoch_26.csv"
         train_dataset = ChexpertDataset(csv_file=train_csv, train_base_path=baase, test_base_path=baase2, transform=transform_train, train_class=args.train_class, train=True, fc_results=fc_head_results, correct_support_only=False)
-        # train_dataset_correct_only = ChexpertDataset(csv_file=train_csv, train_base_path=baase, test_base_path=baase2, transform=transform_train, train_class=args.train_class, train=True, fc_results=fc_head_results, correct_support_only=True)
+        train_dataset_correct_only = ChexpertDataset(csv_file=train_csv, train_base_path=baase, test_base_path=baase2, transform=transform_train, train_class=args.train_class, train=True, fc_results=fc_head_results, correct_support_only=True)
         val_dataset = ChexpertDataset(csv_file=test_csv, train_base_path=baase, test_base_path=baase2, transform=transform_test, train_class=args.train_class, train=False)
         print("initialized datasets")
-        # train_dataset.num_classes = 2
+        train_dataset.num_classes = 2
         
         genders = train_dataset.genders
+        args.correct_support_only = False
         if args.correct_support_only:
             train_dataset_correct_only.num_classes = 2
             genders = train_dataset_correct_only.genders
