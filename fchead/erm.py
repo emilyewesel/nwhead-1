3c201a890d8b05a694a822a3568dd50ab54bd61a
emilyewesel <ewesel@stanford.edu>
2024-05-24 14:52:32 -0700

    Update erm.py

--- fchead/erm.py
+++ fchead/erm.py
@@ -184,7 +184,8 @@ class GroupDRO(ERM):
 
     def _compute_loss(self, i, x, y, a, step):
         losses = self.loss(self.predict(x), y)
-
+        print("y", y.type, y)
+        print("a", a.type, a)
         for idx_g, idx_samples in self.return_groups(y, a):
             self.q[idx_g] *= (self.hparams["groupdro_eta"] * losses[idx_samples].mean()).exp().item()
 
