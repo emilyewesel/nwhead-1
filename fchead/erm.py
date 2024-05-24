8a14de220d5ffdcdeb0ed575fc84450d978958c7
emilyewesel <ewesel@stanford.edu>
2024-05-24 14:57:50 -0700

    Update erm.py

--- fchead/erm.py
+++ fchead/erm.py
@@ -185,8 +185,9 @@ class GroupDRO(ERM):
     def _compute_loss(self, i, x, y, a, step):
         losses = self.loss(self.predict(x), y)
         print("y", y.type, y)
-        a = torch.tensor(a)
-        print("a", a.type, a)
+        
+        print("a", a)
+        a = a.astype(y.type)
         for idx_g, idx_samples in self.return_groups(y, a):
             self.q[idx_g] *= (self.hparams["groupdro_eta"] * losses[idx_samples].mean()).exp().item()
 
